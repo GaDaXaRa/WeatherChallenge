08df739e604db016f284fd6bff98973293c0fa4f
gadaxara <gadaxara@gmail.com>
2017-11-29 18:41:46 +0100

    Initial Commit

---
+++ WeatherChallenge/WeatherChallenge.h
@@ -0,0 +1,19 @@
+//
+//  WeatherChallenge.h
+//  WeatherChallenge
+//
+//  Created by Miguel Santiago Rodríguez on 29/11/17.
+//  Copyright © 2017 Miguel Santiago Rodríguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeatherChallenge.
+FOUNDATION_EXPORT double WeatherChallengeVersionNumber;
+
+//! Project version string for WeatherChallenge.
+FOUNDATION_EXPORT const unsigned char WeatherChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherChallenge/PublicHeader.h>
+
+
